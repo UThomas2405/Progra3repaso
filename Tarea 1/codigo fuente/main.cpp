49b80c02e11918b88a1873ea008cabd257c97935
estebanSulcaInfante <esteban.sulca@utec.edu.pe>
2024-08-10 16:14:12 -0500

    tarea1: cstring, sobrecarga istream ostream

--- Tarea 1/codigo fuente/main.cpp
+++ Tarea 1/codigo fuente/main.cpp
@@ -1,30 +1,64 @@
 #include <iostream>
-
+#include <cstring> // para usar strcpy y strlen
 class Text{
 private:
-    std::string * txt;
+    size_t size;
+    int max_size;
+    char * txt;
 public:
-    Text(const char * _txt){ // const char * === "HOLA MUNDO"
-
-        txt = new std::string(_txt); // constructor copia de std::string
+    // constructor por defecto
+    Text(){ size=0; max_size= 1024; txt= nullptr;}
+    Text(const char * _txt){
+        max_size= 1024;
+        size = strlen(_txt); //revisar para implementarlo from scratch
+        txt = new char[size];
+        strcpy(txt,_txt); //revisar para implementarlo from scratch
     }
 
-
     friend std::ostream& operator <<(std::ostream& os, Text txt);
+    friend std::istream& operator >>(std::istream& is, Text& txt);
 };
 
 std::ostream& operator <<(std::ostream& os, Text txt){
 
-    os << *txt.txt;
+    os<<txt.txt;
 
     return os;
 }
 
+std::istream& operator >>(std::istream& is, Text& txt) {
+
+
+    // guardar lo que esta adentro del is en el objeto txt
+    char buffer[1000];
+    is >> buffer;
+
+    delete[] txt.txt; // liberar memoria actual si existe
+
+    txt.txt = new char[strlen(buffer) + 1];
+    strcpy(txt.txt, buffer);
+
+    return is;
+}
+
+void test1(){
+    Text text_1 = "Hola Mundo";
+    std::cout << text_1 << std::endl;
+}
+
+void test2(){
+    Text text_1;
+    std::cin >> text_1;
+    std::cout << text_1 << std::endl;
+}
+
 int main() {
 
-    Text text_1 = "Hola Mundo123124";
-    std::cout << text_1 << std::endl;
-
+    test2();
+//    std::string x="hola mundo";
+//    std::cout << "size: "<< x.size();
+//    std::cout << "capacidad: "<< x.capacity();
+//    std::cout << "max_size: "<< x.max_size();
 
 
     return 0;
